957ede547e46ec118b63baf283a56ebfa340f3c3
picosaur <picosaur@tuta.io>
2026-03-27 19:55:39 +0300

    repl-vite fixes

--- repl-vite/vite.config.js
+++ repl-vite/vite.config.js
@@ -3,22 +3,20 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/mlab/',          // ← add this line
   build: {
     outDir: 'dist',
     sourcemap: true,
   },
   server: {
     port: 3000,
-    // Headers needed for SharedArrayBuffer (if you use threads later)
     headers: {
       'Cross-Origin-Opener-Policy': 'same-origin',
       'Cross-Origin-Embedder-Policy': 'require-corp',
     },
   },
-  // Treat WASM glue code as external asset
   assetsInclude: ['**/*.wasm'],
   optimizeDeps: {
-    // Don't try to bundle the Emscripten glue
     exclude: ['mlab_repl'],
   },
 });
